ed2e12b42f1a339aee520a2332ad6b724a34db1b
Zafeer <mohammadzafeer2610@gmail.com>
2024-07-18 14:01:28 +0530

    home path updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from './pages/home'
+// import Home from './pages/home'
+import Home  from '../src/pages/Home'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
 import Signup from './pages/Signup'
